d51173e5e03e3da39f0ee8e319dfa9b40bdd759f
Algo11 <dokkersprofic182@gmail.com>
2026-05-03 17:46:16 +0500

    habr_pars_50page

--- cmd/habr-parser.py
+++ cmd/habr-parser.py
@@ -5,21 +5,26 @@ from utils import check_and_crate_result
 
 URL = 'https://habr.com/ru/feed/'
 
-# article link
-# article headers
-# article views
-#article tags
-def parse_article(article):
-    header = article.find('h2', class_='tm-title')
 
-    if header == None:
-        raise ValueError('Article do not have header. SKIP...')
+def parse_article(soup):
+    articles_list = soup.find_all('article', class_='tm-articles-list__item')
+    print(f'Parsing articles..., find {len(articles_list)} articles')
+    data = []
 
-    header_text = header.find('span').text
-    article_link = header.find('a').attrs['href']
-    article_views = article.find('span', class_='tm-icon-counter__value').text
-    # article_tags = list(map(lambda tag: tag.find('span').text, article.find('div', class_='tm-publication-hubs')))
-    return { 'header_text' : header_text, 'article_link' : article_link, 'article_views' : article_views}
+    for article in articles_list:
+        header = article.find('h2', class_='tm-title')
+        if header is None:
+            raise ValueError('Article do not have header. SKIP...')
+        header_text = header.find('span').text
+        article_link = header.find('a').attrs['href']
+        article_views = article.find('span', class_='tm-icon-counter__value').text
+        data.append ({ 
+            'header_text' : header_text, 
+            'article_link' : article_link, 
+            'article_views' : article_views
+        })
+
+    return data
 
 def main():
     req = requests.get(URL, headers={
@@ -27,13 +32,35 @@ def main():
     })
     if req.status_code == 200:
         soup = BeautifulSoup(req.content, 'html.parser')
-        articles_list = soup.find_all('article', class_='tm-articles-list__item')
         parsed_articles = []
-        for a in articles_list:
-            try:
-                parsed_articles.append(parse_article(a))
-            except ValueError as e:
-                print(e)
+        try:
+            parsed_articles.append(parse_article(soup))
+        except ValueError as e:
+            print(e)
+
+        count_page = soup.find_all('a', class_='tm-pagination__page')
+        count_page = int(count_page[-1].text)
+        print(f'Found {count_page} pages')
+
+        need_pages = min(5, count_page)
+
+        for i in range(2, need_pages + 1):
+            url = f'{URL}/{i}'
+            print(f'Parsing page {i}...')
+            
+            time.sleep(5)
+            req = requests.get(url, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36'
+            })
+            if req.status_code == 200:
+                try:
+                    parsed_articles.append(parse_article(soup))
+                except ValueError as e:
+                    print(e)
+            else:
+                print(f'Parsing page {i} error SKIP')
+                continue
+        
         
         result_path = check_and_crate_result()
 
